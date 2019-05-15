6e1714f33ebf2de9ae95a45d9a8e4312211d31ed
RobStein96 <49456466+RobStein96@users.noreply.github.com>
2019-05-14 22:53:00 -0700

    emerge window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
+import {NgbdModalBasic} from './modal-example'; // Implementacion de Modal Popup
+
 @Component({
   selector: 'app-root',
+  template: <br><ngbd-modal-example></ngbd-modal-example>,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
